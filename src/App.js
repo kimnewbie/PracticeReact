6599cf1404fc5a82faa9763436ad90d2e00ebc93
kimnewbie <gracecancho@gmail.com>
2021-08-25 23:11:04 +0900

    side menu ordering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DataTableSearch from './dataTableSearch/DataTableSearch';
+import McSideMenu from './mcdonalds/McSideMenu';
 
 function App() {
   return (
     <div>
       {/* <DataTableSearch /> */}
+      <McSideMenu />
     </div>
   );
 
---
+++ src/mcdonalds/McSideMenu.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import './mcSideMenu.css'
+
+export default function McSideMenu() {
+    const sideMenus = [
+        "감자 튀김 🍟",
+        "콜라 🥤",
+        "애플 파이 🥧",
+        "소프트 아이스크림 🍦",
+        "선택하지 않음"
+    ]
+    const [orders, setOrders] = useState([]);
+    const onClickSideMenu = (selectedItem) => {
+        if (selectedItem === sideMenus[4]) {
+            setOrders([])
+        }
+        if (orders.includes(selectedItem)) {
+            setOrders(orders => orders.filter(order => order !== selectedItem));
+            return;
+        }
+        if (orders.includes("선택하지 않음")) {
+            setOrders(orders => orders.filter(order => order !== "선택하지 않음"))
+        }
+        setOrders(orders => [...orders, selectedItem])
+    }
+
+    useEffect(() => {
+        console.log(orders)
+    }, [orders])
+
+    return (
+        <div className="chooseMcMenus">
+            <h1>사이드 메뉴를 선택하세요</h1>
+            <ul>
+                {
+                    sideMenus.map(((sideMenu, index) => (
+                        <li
+                            className={orders.find(order => order === sideMenu)
+                                ? "menu-item active"
+                                : "menu-item"}
+                            key={index}
+                            onClick={() => onClickSideMenu(sideMenu)}
+                        >
+                            {sideMenu}
+                        </li>
+                    )))
+                }
+            </ul>
+        </div >
+    )
+}
