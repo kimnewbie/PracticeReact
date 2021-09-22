a24d32ae65aa34d9192b9bc5476cbdc6501882b0
kimnewbie <gracecancho@gmail.com>
2021-09-22 21:39:23 +0900

    added reactBigCalendar and reactDatePicker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import InputSorting from './components/InputSorting';
 
 function App() {
   return (
     <div>
-      <InputSorting />
     </div>
   );
 
