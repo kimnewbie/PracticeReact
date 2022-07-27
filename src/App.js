242cf2f89ca418693c84fbc1ff71c3e3e3a43bbc
youjinkim <gracecancho@myatman.com>
2022-07-27 17:53:22 +0900

    cursor 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import ScrollBox from './fromBook/ScrollBox';
+import McSideMenu from './mcdonalds/McSideMenu';
 
 function App() {
   return (
     <div className='appContainer'>
-      <ScrollBox />
+      <McSideMenu />
     </div>
   );
 
