08417c4955e9f87cb4c40d99ea13763004bc3d2b
youjinkim <gracecancho@myatman.com>
2022-07-29 13:44:46 +0900

    classnames 사용

--- src/fromBook/CssModule.js
+++ src/fromBook/CssModule.js
@@ -1,12 +1,37 @@
 import styles from './CSSModule.module.css'// rsc
+import classNames from 'classnames/bind'; // yarn add classnames
+
+const MyComponent = ({ hightlighted, theme }) => {
+    classNames('one', 'two'); // 'one two'
+    classNames('one', { two: true }); // 'one two'
+    classNames('one', { two: false }); // 'one'
+    classNames('one', ['two', 'three']); // 'one two three'
+
+    const myClass = 'hello';
+    classNames('one', myClass, { myCondition: true }); // = 'one hello myCondition'
+
+    return (
+        <>
+            <div className={`MyComponent ${theme} ${hightlighted ? 'hightlited' : ''}`}>Complicated</div>
+            <div className={classNames('MyComponent', { hightlighted }, theme)}>Simple</div>
+        </>
+    )
+}
 
 const CssModule = () => {
+    const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정하고
+
     return (
-        // <div className={styles.wrapper}>
-        // <div className={`${styles.wrapper} ${styles.inverted}`}>
-        <div className={[styles.wrapper, styles.inverted].join(' ')}>
-            안녕하세요, 저는 < span className='something' > CSS Module!</span >
-        </div >
+        <>
+            {/* <div className={styles.wrapper}> */}
+            {/* <div className={`${styles.wrapper} ${styles.inverted}`}> */}
+            {/* <div className={[styles.wrapper, styles.inverted].join(' ')}> */}
+            <div className={cx('wrapper', 'inverted')}>
+                안녕하세요, 저는 < span className='something' > CSS Module!</span >
+            </div >
+            <br />
+            <MyComponent />
+        </>
     );
 };
 
