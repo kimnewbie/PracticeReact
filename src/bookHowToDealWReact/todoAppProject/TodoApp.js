51f8662e0011332d5daaf0f9c63ef3e1fc2f7152
youjinkim <gracecancho@myatman.com>
2022-08-01 09:57:53 +0900

    D기능 추가

--- src/bookHowToDealWReact/todoAppProject/TodoApp.js
+++ src/bookHowToDealWReact/todoAppProject/TodoApp.js
@@ -45,13 +45,23 @@ const TodoApp = () => {
             setTodos(todos.filter(todo => todo.id !== id));
         },
         [todos],
-    )
+    );
+
+    const onToggle = useCallback(
+        id => {
+            setTodos(
+                todos.map(todo =>
+                    todo.id === id ? { ...todo, checked: !todo.checked } : todo)
+            )
+        },
+        [todos],
+    );
 
     return (
         <div>
             <TodoTemplate>
                 <TodoInsert onInsert={onInsert} />
-                <TodoList todos={todos} onRemove={onRemove} />
+                <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
             </TodoTemplate>
         </div>
     );
--- src/bookHowToDealWReact/todoAppProject/TodoListItem.js
+++ src/bookHowToDealWReact/todoAppProject/TodoListItem.js
@@ -8,12 +8,15 @@ import cn from 'classnames';
 
 
 // https://react-icons.github.io/react-icons/icons?name=md
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
     const { id, text, checked } = todo;
 
     return (
         <div className='TodoListItem'>
-            <div className={cn('checkbox', { checked })}>
+            <div
+                className={cn('checkbox', { checked })}
+                onClick={() => onToggle(id)}
+            >
                 {
                     checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />
                 }
